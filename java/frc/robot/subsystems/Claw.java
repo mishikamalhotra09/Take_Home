755f067aa3054363b81d0500f8bace397653de2b
Mishika Malhotra <malhotra.mishika@gmail.com>
2024-12-01 14:56:59 -0500

    Edited classes into final versions

--- java/frc/robot/subsystems/Claw.java
+++ java/frc/robot/subsystems/Claw.java
@@ -4,25 +4,19 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
-import com.revrobotics.CANSparkMax;
-
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Claw extends SubsystemBase {
-  private WPI_VictorSPX leftMotor = new WPI_VictorSPX(port);
-  private WPI_VictorSPX rightMotor = new WPI_VictorSPX(port);
   private Compressor c = new Compressor();
-  private Solenoid rightClaw = new Solenoid(port);
-  private Solenoid leftClaw = new Solenoid(port);
-  CANSparkMax controller = new CANSparkMax();
-  private int BTN_A = 0;
+  private Solenoid rightClaw = new Solenoid(0);
+  private Solenoid leftClaw = new Solenoid(1);
+  private Joystick controller = new Joystick(0);
+  private int BTN_1 = 0;
 
 
   public Claw() {
@@ -30,7 +24,7 @@ public class Claw extends SubsystemBase {
     toggleClawButton.whenPressed(toggleClaw);
   }
 
-  JoystickButton toggleClawButton = new JoystickButton(controller, 0);
+  JoystickButton toggleClawButton = new JoystickButton(controller, BTN_1);
 
   public static Command toggleClaw = InstantCommand(() -> subsystems.Claw.toggle(), subsystems.Claw);
 
--- java/frc/robot/subsystems/DriveTrain.java
+++ java/frc/robot/subsystems/DriveTrain.java
@@ -5,43 +5,39 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Command.RunCommand;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSPX;
 import frc.robot.Robot;
 import edu.wpi.first.wpilibj.Utility;
+import edu.wpi.first.wpilibj.Joystick;
 
 
 public class DriveTrain extends SubsystemBase {
-  private WPI_TalonSRX rightWheels, leftWheels;
-  private DifferentialDrive driveSystem = new DifferentialDrive(leftWheels, rightWheels);
-  private CANSparkMax controller = new CANSparkMax (deviceID, motorType);
-  private double speed, forward, rotation;
-  private double[] values;
+
+  private CANSparkMax frontRight = new CANSparkMax(1, motorType);
+  private CANSparkMax backRight = new CANSparkMax(2, motorType);
+  private CANSparkMax frontLeft = new CANSparkMax(3, motorType);
+  private CANSparkMax frontLeft = new CANSparkMax(4, motorType);
+  private Joystick controller = new Joystick(0);
+  private DifferentialDrive drive;
+  private double forward, rotation;
   private Robot robot;
 
+  public static Command run15Secs = new RunCommand(driveSystem.arcadeDrive(forward, 0));
 
   public DriveTrain(){
-    arcadeValues(controller);
-    long millisecondsToRun = 15000;
+    arcadeValues();
+    backRight.follow(frontRight);
+    backLeft.follow(frontLeft);
+    drive = new DifferentialDrive(frontLeft, frontRight);
+    long msToRun = 15000;
     long startingTime = Utility.getFPGATime();
     while (Utility.getFPGATime() - startingTime <= millisecondsToRun){
       run15Secs.excecute();
     robot.rotateBy(180);
-    
+    }
   }
 
-  public static Command run15Secs = new RunCommand(driveSystem.arcadeDrive(forward, 0));
-
-
-  public void arcadeValues(CANSparkMax controller){
-    speed = controller.get();
-    values = controller.ControlType[] values();
-    rotation = values[1];
-    forward = Math.sqrt(Math.pow(speed, 2) - Math.pow(forward, 2));
-
-    driveSystem.arcadeDrive(-forward, -rotation);
-
+  public arcadeValues(){
+    forward = -controller.getY();
+    rotation = -controller.getX();
   }
-
-  }  
-
 }
